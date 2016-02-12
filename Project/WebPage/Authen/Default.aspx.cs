9e597e5f822adf3517bbc51192e82fc1aa5d1754
sittiporn <sittiporn@silkspan.local>
2016-02-12 17:02:30 +0700

    update code

---
+++ Project/WebPage/Authen/Default.aspx.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class WebPage_Authen_Default : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+      //  Response.Write(Session.SessionID +"<br>"+Guid.NewGuid());
+    }
+}
